05d3936cdb3d292a30c96bb34057b1360acd419f
SauloBrandao <2023211510080@iesp.edu.br>
2025-11-03 01:11:10 -0300

    Feat#1: Criando Repositorio

---
+++ src/main/java/application/PsEstoqueApiApplication.java
@@ -0,0 +1,13 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsEstoqueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsEstoqueApiApplication.class, args);
+	}
+
+}
